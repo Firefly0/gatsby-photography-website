ed32e039adab26b3f1157c1e80be9405d5785671
Ioan Agrigoroaei <ioan.agrigoroaei@yahoo.com>
2019-03-20 19:40:04 +0200

    added navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "react-bootstrap/dist/react-bootstrap.min.js"
 // import "./index.css"
 
 const IndexPage = () => (
@@ -15,6 +16,7 @@ const IndexPage = () => (
       <div>
         <div
           style={{
+            height: "900px",
             maxWidth: `300px`,
             margin: "0 auto 1.45rem",
           }}
