e9a126b5d326fae86274918dd1bc371c5bd68d2f
Ioan Agrigoroaei <ioan.agrigoroaei@yahoo.com>
2019-04-10 22:22:08 +0300

    refactor

--- src/pages/sedintaFoto.js
+++ src/pages/sedintaFoto.js
@@ -55,17 +55,19 @@ class SedintaFoto extends Component {
                   >
                     {album.node.imagini.map((element, indexPhoto) => {
                       return (
-                        <img
-                          className="imageAlbums"
-                          src={element.file.url}
-                          onClick={() => {
-                            this.setState({
-                              indexAlbum,
-                              photoIndex: indexPhoto,
-                              isOpen: true,
-                            })
-                          }}
-                        />
+                        <div>
+                          <img
+                            className="imageAlbums"
+                            src={element.file.url}
+                            onClick={() => {
+                              this.setState({
+                                indexAlbum,
+                                photoIndex: indexPhoto,
+                                isOpen: true,
+                              })
+                            }}
+                          />
+                        </div>
                       )
                     })}
                   </div>
